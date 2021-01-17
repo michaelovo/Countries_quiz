8815dfc70fbed2a51d145fe5ab11e5a21d696f3e
michael <emikeovo@gmail.com>
2021-01-17 07:20:15 +0100

    Added search field and icon to appbar

--- lib/screens/AllCountries.dart
+++ lib/screens/AllCountries.dart
@@ -6,18 +6,17 @@ import 'package:country_house/screens/Country.dart';
 import 'package:dio/dio.dart';
 
 class AllCountries extends StatefulWidget {
-
   @override
   _AllCountriesState createState() => _AllCountriesState();
 }
 
 class _AllCountriesState extends State<AllCountries> {
   Future<List> countries;
-  Future<List> getCountries()async{
+  bool isSearching = false;
+  Future<List> getCountries() async {
     var response = await Dio().get("https://restcountries.eu/rest/v2/all");
     // print(response.data.length);
     return response.data;
-
   }
 
   @override
@@ -34,42 +33,72 @@ class _AllCountriesState extends State<AllCountries> {
       appBar: AppBar(
         backgroundColor: Colors.pink,
         elevation: 0,
-        title: Text(
-          'All countries',
-          style: TextStyle(
-            letterSpacing: 1.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+        title: !isSearching
+            ? Text(
+                'All countries',
+                style: TextStyle(
+                  letterSpacing: 1.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              )
+            : TextField(
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            hintText: "Search country here ...",
+            hintStyle: TextStyle(
+                color: Colors.white
+            ),
           ),
         ),
+        actions: <Widget>[
+          isSearching ?
+          IconButton(
+              icon: Icon(Icons.cancel),
+              onPressed: () {
+                setState(() {
+                  this.isSearching = false;
+                });
+              }):
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  this.isSearching = true;
+                });
+              }),
+        ],
         centerTitle: true,
       ),
       body: Container(
         padding: EdgeInsets.all(10),
         child: FutureBuilder<List>(
-          future : countries,
-          builder: (BuildContext context, AsyncSnapshot<List> snapshot){
-            if(snapshot.hasData){
+          future: countries,
+          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+            if (snapshot.hasData) {
               // print(snapshot.data);
               return ListView.builder(
-                itemBuilder: (BuildContext context, int index){
-                  return GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => Country(snapshot.data[index]),
-                              ),
-                            );
-                          },
-                          child: Card(
-                              elevation: 10,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(vertical:10.0, horizontal: 8.0),
-                                child: Text(snapshot.data[index]["name"]),
-                              )
-                          ),
-                        );
-
+                  itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => Country(snapshot.data[index]),
+                      ),
+                    );
+                  },
+                  child: Card(
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 8.0),
+                        child: Text(snapshot.data[index]["name"]),
+                      )),
+                );
               });
             }
             return null;
